5b5df83145c0d9b7bcc853fbb4e2f44d9a38397b
seven <zhangmin145440@ehousechina.com>
2018-06-14 17:36:33 +0800

    add

--- pages/question/question.js
+++ pages/question/question.js
@@ -1,7 +1,10 @@
 // import util from './../../utils/util.js';
 Page({
     data: {
-        
+        showtab:0,  //顶部选项卡索引
+        showfootertab:0,  //底部标签页索引
+        uploadimgs:[], //上传图片列表
+        editable: false //是否可编辑
     },
     onLoad() {//加载数据渲染页面
         
